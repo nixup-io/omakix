021fede42f6d9fb1c2462bc7f6c4d99fcb9e2cdd
Michael Smith <m@michaelsmith.be>
2024-06-14 15:13:06 +0200

    Remove problematic overlay

--- test/gui-vm.nix
+++ test/gui-vm.nix
@@ -55,22 +55,6 @@
         epiphany # web browser
       ]);
 
-    nixpkgs.overlays = [
-      (final: prev: {
-        gnome = prev.gnome.overrideScope (
-          gnomeFinal: gnomePrev: {
-            mutter = gnomePrev.mutter.overrideAttrs (old: {
-              src = pkgs.fetchgit {
-                url = "https://gitlab.gnome.org/vanvugt/mutter.git";
-                rev = "94f500589efe6b04aa478b3df8322eb81307d89f";
-                sha256 = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
-              };
-            });
-          }
-        );
-      })
-    ];
-
     security.sudo.wheelNeedsPassword = false;
     users.users.demo = {
       createHome = true;
