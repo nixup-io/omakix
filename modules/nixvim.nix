f5479d52522dcf17c503692a6fab0084e7b00c82
Michael Smith <m@michaelsmith.be>
2024-06-14 12:55:39 +0200

    Use NixVim

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -9,9 +9,26 @@ in {
   config = lib.mkIf cfg.enable {
     programs.nixvim = {
       enable = true;
-
       colorschemes.tokyonight.enable = true;
-      plugins.transparent.enable = true;
+      globals.mapleader = " ";
+      opts = {
+        number = true;
+        shiftwidth = 2;
+        cursorline = true;
+      };
+      plugins = {
+        lualine.enable = true;
+        neo-tree.enable = true;
+        telescope.enable = true;
+        treesitter.enable = true;
+        noice.enable = true;
+        lsp = {
+          enable = true;
+          servers = {
+            ruby-lsp.enable = true;
+          };
+        };
+      };
     };
   };
 }
