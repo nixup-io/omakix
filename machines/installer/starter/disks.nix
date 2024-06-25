3d8916ccd1bf1873f078e179f565c98b6fa828ae
Michael Smith <m@michaelsmith.be>
2024-06-25 11:34:16 +0200

    Recovery commit 5

---
+++ machines/installer/starter/disks.nix
@@ -0,0 +1,40 @@
+{
+  disko.devices = {
+    disk = {
+      "${builtins.readFile /tmp/diskname}" = {
+        type = "disk";
+        device = "/dev/${builtins.readFile /tmp/diskname}";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                settings = {
+                  allowDiscards = true;
+                  keyFile = "/tmp/secret.key";
+                };
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
